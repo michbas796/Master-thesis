fde42d606030b6dc96d90a7e8a949f9eb7b23744
Michał Basak <michal.b1990@gazeta.pl>
2016-07-19 18:37:09 +0200

    Kolejny etap implementacji ładowania grafu z pliku oraz implementacja wykrywania ograniczeń dotyczących kolejności miast.

--- sop/src/main/java/michal/basak/sop/genetic_algorithm/CitiesGraph.java
+++ sop/src/main/java/michal/basak/sop/genetic_algorithm/CitiesGraph.java
@@ -5,41 +5,72 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CitiesGraph {
     public static final int PRECEDENCE_CONSTRAINT = -1;
     public static final int NO_EDGE = 0;
-    private ArrayList<ArrayList<Integer>> adjacencyMatrix;
+    private List<List<Integer>> adjacencyMatrix;
+    private List<Set<Integer>> precedenceConstraints;
     
     public CitiesGraph(File inputFile) {       
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFile))) {
-            Scanner scanner;
-            String currentLine;        
-            while ((currentLine = fileReader.readLine()) != null) {
-                scanner = new Scanner(currentLine);
-                ArrayList<Integer> adjacencyMatrixRow = null;
-                while (scanner.hasNextInt()) {                    
-                    adjacencyMatrixRow = new ArrayList<>();
-                    adjacencyMatrixRow.add(scanner.nextInt());
-                }
-                scanner.close();
-                if (adjacencyMatrixRow != null && adjacencyMatrixRow.size() > 1) {
-                    adjacencyMatrix.add(adjacencyMatrixRow);
-                }           
-            } 
-        } catch (IOException e) {
-            //TODO prawdopodobnie powinno wyżucić nowy wyjątek.
-        }
+        loadAdjacencyMatrixFromFile(inputFile);
+        assignPrecedenceConstraints();
     }
     
     public int getEdgeWeight(int startNode, int endNode) {
         return adjacencyMatrix.get(startNode).get(endNode);
     }
     
-    public int[] getRandomHamiltonianPath() {
+    public int[] getRandomHamiltonianPath(int startNode, int endNode) {
+        int numberOfNodes = adjacencyMatrix.size();
         //TODO
         return new int[10];
     }
-        
+    
+    public int numberOfCities() {
+        return adjacencyMatrix.size();
+    }
+    
+    private void loadAdjacencyMatrixFromFile(File inputFile) {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFile))) {            
+            String currentLine;               
+            adjacencyMatrix = new ArrayList<>();
+            while ((currentLine = fileReader.readLine()) != null) {
+                parseFileLine(currentLine);                         
+            }            
+        } catch (IOException e) {
+            //TODO prawdopodobnie powinno wyżucić nowy wyjątek.
+        }
+    }
+    
+    private void parseFileLine(String fileLine) {
+        ArrayList<Integer> adjacencyMatrixRow = new ArrayList<>();
+        try (Scanner scanner = new Scanner(fileLine)) {            
+            while (scanner.hasNextInt()) {
+                adjacencyMatrixRow.add(scanner.nextInt());
+            }
+        }
+        if (adjacencyMatrixRow.size() > 1) {
+            adjacencyMatrix.add(adjacencyMatrixRow);
+        }  
+    }
+      
+    private void assignPrecedenceConstraints() {
+        precedenceConstraints = new ArrayList<>();
+        for (int rowNum = 0; rowNum < adjacencyMatrix.size(); rowNum++) {
+            List<Integer> row = adjacencyMatrix.get(rowNum);
+            Set<Integer> currentCityPrecedenceConstraints = new HashSet<>();
+            for (int colNum = 0; colNum < adjacencyMatrix.size(); colNum++) {
+                if (row.get(colNum) == PRECEDENCE_CONSTRAINT) {
+                    currentCityPrecedenceConstraints.add(colNum);
+                }
+            }
+            precedenceConstraints.add(currentCityPrecedenceConstraints);
+        }
+    }
+               
 }
--- sop/src/main/java/michal/basak/sop/genetic_algorithm/Individual.java
+++ sop/src/main/java/michal/basak/sop/genetic_algorithm/Individual.java
@@ -4,7 +4,7 @@ public class Individual {
     private int fitness;
     private int[] chromosome;
     public Individual(CitiesGraph citiesGraph) {
-        chromosome = citiesGraph.getRandomHamiltonianPath();
+        chromosome = citiesGraph.getRandomHamiltonianPath(0, citiesGraph.numberOfCities() - 1);
         fitness = 0;
         for (int i = 0; i < chromosome.length - 1; i++) {
             fitness += citiesGraph.getEdgeWeight(i, i+1);
