72a8ee30aab42d3e3ea372f342dbbc6311b57f3c
Michał Basak <michal.b1990@gazeta.pl>
2016-07-27 12:09:11 +0200

    Drobna optymalizacja

--- sop/src/main/java/michal/basak/sop/genetic_algorithm/RouletteWheelSelector.java
+++ sop/src/main/java/michal/basak/sop/genetic_algorithm/RouletteWheelSelector.java
@@ -1,19 +1,15 @@
 package michal.basak.sop.genetic_algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class RouletteWheelSelector implements IndividualSelector {
     
     @Override
     public Population selectIndividuals(Population population) {
-        Population selectedIndividuals = new Population();
-        population.sortFromWorstToBest();
-        List<Double>distributionFuction = evaluateDistributionFunction(population);
+        Population selectedIndividuals = new Population();       
+        double[] distributionFunction = evaluateDistributionFunction(population);
         for (int i = 0; i < population.size(); i++) {
             double randomNumber = Math.random();
-            for (int j = 0; j < distributionFuction.size(); j++) {
-                if (randomNumber < distributionFuction.get(j)) {
+            for (int j = 0; j < distributionFunction.length; j++) {
+                if (randomNumber < distributionFunction[j]) {
                     selectedIndividuals.add(population.getIndividual(j));
                     break;
                 }
@@ -22,17 +18,17 @@ public class RouletteWheelSelector implements IndividualSelector {
         return selectedIndividuals;
     }
    
-    private static List<Double> evaluateDistributionFunction(Population population) { 
-        List<Double> probabilities = new ArrayList<>();
+    private static double[] evaluateDistributionFunction(Population population) { 
+        double[] distributionFunction = new double[population.size()];
         int individualsFitnessSum = sumOfIndividualsFitness(population);
         double probability = 1.0 - ((double)(individualsFitnessSum - population.getIndividual(0).getFitness())/(double)individualsFitnessSum);
-        probabilities.add(probability);
+        distributionFunction[0] = probability;
         for (int i = 1; i < population.size(); i++) {
             Individual currentIndividual = population.getIndividual(i);
-            probability = probabilities.get(i - 1) + 1.0 - ((double)(individualsFitnessSum - currentIndividual.getFitness())/(double)individualsFitnessSum);
-            probabilities.add(probability);
+            probability = distributionFunction[i - 1] + 1.0 - ((double)(individualsFitnessSum - currentIndividual.getFitness())/(double)individualsFitnessSum);
+            distributionFunction[i] = probability;
         }
-        return probabilities;
+        return distributionFunction;
     }
     
     private static int sumOfIndividualsFitness(Population population) {
